9e5987f06ad44a6d5b8b2e5ab8fc63a41e754cdb
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2021-01-03 21:29:05 +0200

    refactor (Main.java): rename to Main2.java

--- src/version1/Main2.java
+++ src/version1/Main2.java
@@ -1,11 +1,13 @@
 package version1;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println ("add some code 1");
 
         System.out.println ("some code 2");
+
+        System.out.println ("some code 3");
     }
 }
