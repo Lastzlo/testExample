76f358399e15f14cf46110749d8773d78ed98f73
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2021-01-03 21:22:03 +0200

    feat(Main.java) Add class that have some code

---
+++ src/version1/Main.java
@@ -0,0 +1,11 @@
+package version1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println ("add some code 1");
+
+        System.out.println ("some code 2");
+    }
+}
