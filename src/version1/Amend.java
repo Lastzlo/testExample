eaf834f76d2e5b4df70321a59e23b4257683c8b0
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2021-01-04 11:48:30 +0200

    feat: add Amend.java

---
+++ src/version1/Amend.java
@@ -0,0 +1,9 @@
+package version1;
+
+public class Amend {
+    public static void main (String[] args) {
+        System.out.println ("Текст который забыли добавить");
+        System.out.println ("а также исправить коментарий каммита");
+        System.out.println ("$ git commit --amend");
+    }
+}
