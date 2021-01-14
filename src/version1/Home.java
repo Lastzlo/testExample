cfdba3dc0e0b8dcc8679587ac9c553773460f681
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2021-01-14 10:53:19 +0200

    feat(Home.java): add constructor

--- src/version1/Home.java
+++ src/version1/Home.java
@@ -1,9 +1,17 @@
 package version1;
 
 public class Home {
-    int namOfFloors;
-    String address;
-    boolean openDoor;
+    private int namOfFloors;
+    private String address;
+    private boolean openDoor;
+
+    Home(){
+        this.address = "no adress";
+        this.namOfFloors = 1;
+        this.openDoor = false;
+    }
+
+
 
 }
 
